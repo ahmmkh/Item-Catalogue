8599585c22c28bdee8fc55ee9935e84c8618c598
AhmedElkashef02 <ahmedelkashef2012@gmail.com>
2018-01-19 01:26:45 +0200

    added basic structure for website

--- project.py
+++ project.py
@@ -1,1 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Category, CategoryItem, Base
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///items.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about1.html')
+
+@app.route('/components')
+def components():
+    return render_template('components.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run()
+    app.run(host='0.0.0.0', port=3000)
