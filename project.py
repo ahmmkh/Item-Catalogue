2476f309a473714539d3e1b9dda5d9eb4efab52f
AhmedElkashef02 <ahmedelkashef2012@gmail.com>
2018-01-20 20:42:37 +0200

    fixed the relative js path in category.html

--- project.py
+++ project.py
@@ -5,50 +5,55 @@ from database_setup import Category, CategoryItem, Base
 
 app = Flask(__name__)
 
+# Connect to Database and create database session
 engine = create_engine('sqlite:///items.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Show all categories
 @app.route('/')
 @app.route('/categories')
 def index():
     categories = session.query(Category).order_by(asc(Category.name))
     return render_template('index.html',categories=categories)
 
-@app.route('/categories/<category>')
-def showCategory(category):
-    category = session.query(Category).filter_by(name=category).one()
+# Show a single category with list of items
+@app.route('/categories/<categoryName>')
+def showCategory(categoryName):
+    category = session.query(Category).filter_by(name=categoryName).one()
     category_items = session.query(CategoryItem).filter_by(
     category_id=category.id).all()
     return render_template('category.html',category=category,category_items=category_items)
 
+# Show a single Item
 @app.route('/categories/<category>/<int:item_id>')
 def showItem(category,item_id):
     category = session.query(Category).filter_by(name=category).one()
     item = session.query(CategoryItem).filter_by(id=item_id).one()
     return render_template('category_item.html',category=category,category_item=item)
 
+# Show about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+# Show components page
 @app.route('/components')
 def components():
     return render_template('components.html')
 
+# Show contact page
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+# Show services page
 @app.route('/services')
 def services():
     return render_template('services.html')
 
-@app.route('/project.html')
-def project():
-    return render_template('project.html')
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
